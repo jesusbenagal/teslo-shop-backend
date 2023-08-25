838d26c481030b573fd4fadada5bb9b98bf9597a
Jesús <jesusguerreropaniagua6@gmail.com>
2023-08-25 20:21:43 +0200

    Auth Decorator adn examples

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 import { AuthService } from "./auth.service";
+import { Auth, GetUser, RawHeaders, RoleProtected } from "./decorators";
+import { User } from "./entities/user.entity";
+import { UserRoleGuard } from "./guards/user-role.guard";
+
 import { LoginUserDto, CreateUserDto } from "./dto";
+import { ValidRoles } from "./interfaces";
 
 @Controller("auth")
 export class AuthController {
@@ -15,5 +21,40 @@ export class AuthController {
   @Post("login")
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get("private")
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser("email") userEmail: string,
+    @RawHeaders() rawHeaders: string[]
+  ) {
+    return {
+      ok: true,
+      message: "This is a private route",
+      user,
+      userEmail,
+      rawHeaders,
+    };
+  }
+
+  @Get("private2")
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get("private3")
+  @Auth(ValidRoles.admin)
+  privateRoute3(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
   }
 }
