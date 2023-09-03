6ad8e82377db3e65fdf9a393fd10f9ff55999cf2
Jesús <jesusguerreropaniagua6@gmail.com>
2023-09-03 14:01:11 +0200

    Created Resource WebSocket

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { ProductsModule } from "./products/products.module";
 import { CommonModule } from "./common/common.module";
 import { SeedModule } from "./seed/seed.module";
 import { FilesModule } from "./files/files.module";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { MessagesWsModule } from "./messages-ws/messages-ws.module";
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -37,6 +39,8 @@ import { AuthModule } from './auth/auth.module';
     FilesModule,
 
     AuthModule,
+
+    MessagesWsModule,
   ],
 })
 export class AppModule {}
